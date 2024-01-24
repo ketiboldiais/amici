0a9fa2cb4d5b21c16951df1e695c444ea7586c7a
Ketib Oldiais <numerstack@gmail.com>
2024-01-24 13:44:47 -0600

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import createMDX from '@next/mdx';
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
